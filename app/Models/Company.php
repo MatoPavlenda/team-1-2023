f21a130331734f8d99c18984d545738283fcc076
Fmladek <kostalr.roman@gmail..com>
2023-11-30 21:52:09 +0100

    revert of comment

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
-{ //Test laravel comitu
+{
     protected $fillable = [
         'name',
         'street',
