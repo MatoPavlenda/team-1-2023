1f9a8193ff34dec0bd71229179d9e538ebbfa8a1
Fmladek <kostalr.roman@gmail..com>
2023-12-12 00:56:22 +0100

    Practice table creation

--- app/Models/Practice.php
+++ app/Models/Practice.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Practice extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $table = 'practice';
+    protected $fillable = [
+        'student_id',
+        'practice_offer_id',
+        'company_employee_id',
+        'title',
+        'description',
+        'startDate',
+        'endDate',
+        'active',
+        'finished',
+        'feedback',
+    ];
 }
