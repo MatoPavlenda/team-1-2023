2c3273d4d4af955694057302cc940db72295759b
marek5816 <marek5816@azet.sk>
2023-12-14 22:46:04 +0100

    Created authentication

---
+++ app/Variables.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: John
+ * Date: 14-Dec-2023
+ * Time: 9:46 PM
+ */
+
+namespace App;
+
+
+class Variables
+{
+    public $admin = "admin";
+    public $student = "student";
+    public $ukfEmployee = "ukf_employee";
+    public $companyEmployee = "company_employee";
+}
